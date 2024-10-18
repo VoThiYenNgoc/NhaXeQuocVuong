c3311e745d48483d12c7a131af32c112dde3a32a
VoThiYenNgoc <VoThiYenNgoc2021@gmail.com>
2024-10-18 22:57:56 +0700

    project

---
+++ NhaXeQuocVuong-main/NhaXe_QuocVuong/Areas/Area_Admin/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NhaXe_QuocVuong.Areas.Area_Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Area_Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
